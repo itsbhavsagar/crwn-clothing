a723e3a60a61de2253a6f453210c84b3bcb13103
Bhavsagar <bhavsagar92@gmail.com>
2025-02-01 16:34:29 +0530

    Added Google Auth & Firebase

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,31 +7,31 @@ const Home = () => {
     {
       id: 1,
       title: 'hats',
-      subtitle: 'shop now',
+      subtitle: 'Shop Now',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'jackets',
-      subtitle: 'shop now',
+      subtitle: 'Shop Now',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
       title: 'sneakers',
-      subtitle: 'shop now',
+      subtitle: 'Shop Now',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
       title: 'womens',
-      subtitle: 'shop now',
+      subtitle: 'Shop Now',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
       title: 'mens',
-      subtitle: 'shop now',
+      subtitle: 'Shop Now',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBV18FiQ6K1l6nyXwGGUBgIrutMMv-jE3w',
+  authDomain: 'crown-clothing-db-6f2e4.firebaseapp.com',
+  projectId: 'crown-clothing-db-6f2e4',
+  storageBucket: 'crown-clothing-db-6f2e4.firebasestorage.app',
+  messagingSenderId: '328832359386',
+  appId: '1:328832359386:web:adc349495324629a6bfee3',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
