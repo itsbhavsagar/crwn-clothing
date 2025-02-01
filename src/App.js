550ae88cd7fb8ee63d52fdd82411f9b3eebe74a5
Bhavsagar <bhavsagar92@gmail.com>
2025-02-01 12:46:26 +0530

    sign-in.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-
+import { Route, Routes, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
-  return <h1> I am the Shop Page!!</h1>;
+  return <h1> I am the Shop Page ✈</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
