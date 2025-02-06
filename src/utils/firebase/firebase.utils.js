540dec8d3406ead4d2a601364a30d11367139a42
Bhavsagar <bhavsagar92@gmail.com>
2025-02-06 14:57:54 +0530

    added sign in firebase utills as well firebase llibraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -39,4 +39,27 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if users data does not exist
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+  return userDocRef();
 };
+
+// if users data exists
+
+// return userDocRef
