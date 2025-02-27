d6c8bb671f6aaa7503ad7f1da5b0058249f2dce3
Bhavsagar <bhavsagar92@gmail.com>
2025-02-27 16:22:26 +0530

    reversed for now

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,41 +1,19 @@
-import { useContext, useCallback, useRef, useEffect } from 'react';
+import { useContext } from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import { CartContext } from '../../contexts/cart.context';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-  const cartIconRef = useRef(null);
 
-  const toggleIsCartOpen = useCallback(() => {
-    setIsCartOpen((prevState) => !prevState);
-  }, [setIsCartOpen]);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (cartIconRef.current && !cartIconRef.current.contains(event.target)) {
-        setIsCartOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [setIsCartOpen]);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div
-      className='cart-icon-container'
-      onClick={toggleIsCartOpen}
-      ref={cartIconRef}
-      role='button'
-      aria-label='Toggle Cart'
-      tabIndex={0}
-    >
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{cartCount}</span>
+      <span className='item-count'>{cartCount} </span>
     </div>
   );
 };
