d7d1177bacd66036e339de6ab65252de97ef422e
Bhavsagar <bhavsagar92@gmail.com>
2025-03-12 00:28:37 +0530

    reused button component in CartDropDown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,11 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
-import './cart-dropdown.styles.scss';
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
@@ -15,16 +19,18 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage> Your cart is empty! </EmptyMessage>
+        )}
+      </CartItems>
       <Button buttonType='inverted' onClick={goToCheckOutHandler}>
         GO TO CHECKOUT
       </Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
